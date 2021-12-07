85c9ddbbee6f68d27b4a83e9f0c44cd0684dbc54
Romain <rom4in@me.com>
2021-12-07 13:41:25 +0000

    Initial Commit

---
+++ beauty/beautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  beautyApp.swift
+//  beauty
+//
+//  Created by Ubicolor on 07/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct beautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
